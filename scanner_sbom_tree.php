3eb9078f7171a7d9a43e3ca421d26777f53c075e
BrandonKveene <fy0457bv@metrostate.edu>
2019-10-07 12:04:37 -0500

    Added jquer-treetable CDN

--- scanner_sbom_tree.php
+++ scanner_sbom_tree.php
@@ -7,12 +7,25 @@
   
  ?>
 
+ <script src="https://cdnjs.com/libraries/jquery-treetable"></script>
+
  <div class="right-content">
     <div class="container">
 
       <h3 style = "color: #01B0F1;">Scanner --> BOM Tree</h3>
 
+      <table>
+        <tr data-tt-id="1">
+          <td>Parent</td>
+        </tr>
+        <tr data-tt-id="2" data-tt-parent-id="1">
+          <td>Child</td>
+        </tr>
+      </table>
+
     </div>
 </div>
 
 <?php include("./footer.php"); ?>
+
+
