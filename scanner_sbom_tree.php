516fdac31accf9820be8d12dc3cc8f711ad7bd0e
BrandonKveene <fy0457bv@metrostate.edu>
2019-10-09 15:19:56 -0500

    Added tree table dependencies/Example code

--- scanner_sbom_tree.php
+++ scanner_sbom_tree.php
@@ -6,16 +6,16 @@
   include("./nav.php");
   
  ?>
-
- <script src="https://cdnjs.com/libraries/jquery-treetable"></script>
-
  <div class="right-content">
     <div class="container">
 
       <h3 style = "color: #01B0F1;">Scanner --> BOM Tree</h3>
 
-      <table>
+      <table id="testTable">
         <tr data-tt-id="1">
+          <span class="indenter" style="padding-left: 0px;">
+            <a href="#" title="Expand">&nbsp;</a>
+          </span>  
           <td>Parent</td>
         </tr>
         <tr data-tt-id="2" data-tt-parent-id="1">
@@ -25,6 +25,14 @@
 
     </div>
 </div>
+
+<link href="./jquery.treetable.css" rel="stylesheet" type="text/css" />
+<link href="./jquery.treetable.theme.default.css" rel="stylesheet" type="text/css" />
+<script src="./jquery.treetable.js"></script>
+
+<script>
+$("#testTable").treetable({expandable: true});
+</script>
 
 <?php include("./footer.php"); ?>
 
