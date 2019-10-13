822790f0c6d87bba7424591ba2637955199de5bf
BrandonKveene <fy0457bv@metrostate.edu>
2019-10-12 19:42:20 -0500

    Test Alternative Tree Building

--- scanner_sbom_tree.php
+++ scanner_sbom_tree.php
@@ -7,27 +7,68 @@
   include("./nav.php");
   
  ?>
+
  <div class="right-content">
     <div class="container">
 
       <h3 style = "color: #01B0F1;">Scanner --> BOM Tree</h3>
-      <table id="testTable">
+      <table id="sbomTable">
       <?php
+      
+      $appArray = array();
+      
+      $sql =  "SELECT app_id,app_name,app_version FROM sbom ORDER BY app_name,app_version ASC;";
+      $result = $db->query($sql);
+
+      if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()){
+          if(!in_array($row["app_id"],$appArray)){
+            $appArray[] = $row["app_id"];
+          }  
+        }    
+      }
+
+      $result->close();
+
+      foreach($appArray as $app){
+        $sql = "SELECT * FROM sbom WHERE app_id = '".$app."' ORDER BY cmp_name ASC;";
+        $result = $db->query($sql);
+        echo $sql . "</br>";
+        if ($result->num_rows > 0) {
+          for($i = 0; $i < $result->num_rows; $i++){
+            $row = $result->fetch_assoc();
+            if($i == 0){
+              echo '<tr data-tt-id="'.$row["app_id"].'">
+                <td>'.$row["app_name"].' '.$row["app_version"].'</td>
+                </tr>';
+            }
+            echo'<tr data-tt-id="'.$row["cmp_name"].'" data-tt-parent-id="'.$row["app_id"].'">
+                <td>'.$row["cmp_name"].' '.$row["cmp_version"].'</td>
+                </tr>';
+          }
+        }
+      }
+      
+      $result->close();
+      
+      /*
       $sql =  "SELECT * FROM sbom ORDER BY row_id ASC;";
           $result = $db->query($sql);
 
           if ($result->num_rows > 0) {
           // output data of each row
               while($row = $result->fetch_assoc()) {
-                  if($pid != $row["app_id"] && $row["app_name"] != "DB_Layer"){
+
+                  if($pid != $row["app_id"]){
                     echo '<tr data-tt-id="'.$row["app_id"].'">
                             <td>'.$row["app_name"].' '.$row["app_version"].'</td>
                           </tr>';       
                   $pid = $row["app_id"];
-                }
-                    echo'<tr data-tt-id="'.$row["cmp_id"].'" data-tt-parent-id="'.$row["app_id"].'">
+                } else {
+                    echo'<tr data-tt-id="'.$row["cmp_name"].'" data-tt-parent-id="'.$row["app_id"].'">
                           <td>'.$row["cmp_name"].' '.$row["cmp_version"].'</td>
                          </tr>';
+                }
                   
               }//end while
           }//end if
@@ -36,6 +77,7 @@
           }//end else
 
        $result->close();
+       */
       ?>
       
 
@@ -49,7 +91,7 @@
 <script src="./jquery.treetable.js"></script>
 
 <script>
-$("#testTable").treetable({expandable: true});
+$("#sbomTable").treetable({expandable: true});
 </script>
 
 <?php include("./footer.php"); ?>
