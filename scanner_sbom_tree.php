1135266e101fc3fbd84061efb1fce086919aba97
JacobLFulton <54561215+JacobLFulton@users.noreply.github.com>
2019-10-14 15:49:51 -0500

    Updated sbom tree

--- scanner_sbom_tree.php
+++ scanner_sbom_tree.php
@@ -4,6 +4,7 @@
   $left_selected = "SBOMTREE";
   global $db;
   global $pid;
+  
   include("./nav.php");
   
  ?>
@@ -18,9 +19,11 @@
 
       <table id="sbomTable">
       <?php
-      /*
+      $count = 0;
+      $cmpArray = array();
       $appArray = array();
-      
+      $nodeArray = array();
+      /*
       $sql =  "SELECT app_id,app_name,app_version FROM sbom ORDER BY app_name,app_version ASC;";
       $result = $db->query($sql);
 
@@ -55,24 +58,60 @@
       
       $result->close();
       */
+      //initial query to populate arrays to check during the primary query
+      $appQuery = "SELECT * from sbom ORDER BY row_id ASC;";
+        $appRes = $db->query($appQuery);
+        if ($appRes->num_rows > 0) {
+          while($row = $appRes->fetch_assoc()) {
+            if($pid != $row["app_id"]){
+              $count = 0;
+              $pid = $row["app_id"];
+            }
+            $nodeArray[$row["app_id"].$count] = 
+            '<tr data-tt-id="'.$row["cmp_id"].'" data-tt-parent-id="'.$row["app_id"].'">
+            <td>'.$row["cmp_name"].' '.$row["cmp_version"].'</td>
+            </tr>';
+            array_push($appArray,$row["app_id"]);
+            array_push($cmpArray,$row["cmp_id"]);
+            $count++;
+          }
+        }
+        else {
+          echo "0 results";
+        }//end else
+
+      $appRes->close();
       
-      $sql =  "SELECT app_id,app_name,app_version,cmp_id,cmp_name,cmp_version FROM sbom ORDER BY app_id,app_name,app_version,cmp_id,cmp_name,cmp_version ASC;";
-      //$sql =  "SELECT * FROM sbom ORDER BY row_id ASC;";
+      //$sql =  "SELECT app_id,app_name,app_version,cmp_id,cmp_name,cmp_version FROM sbom ORDER BY app_id,app_name,app_version,cmp_id,cmp_name,cmp_version ASC;";
+      $sql =  "SELECT * FROM sbom ORDER BY row_id ASC;";
           $result = $db->query($sql);
 
           if ($result->num_rows > 0) {
           // output data of each row
               while($row = $result->fetch_assoc()) {
-                  if($pid != $row["app_id"]){
-                    echo '<tr data-tt-id="'.$row["app_id"].'">
-                            <td>'.$row["app_name"].' '.$row["app_version"].'</td>
-                          </tr>';       
+                if($pid != $row["app_id"] && !in_array($row["app_id"],$cmpArray)){ //creates a new app node if the app_id is not a component
+                  echo '<tr data-tt-id="'.$row["app_id"].'">
+                          <td>'.$row["app_name"].' '.$row["app_version"].'</td>
+                        </tr>';       
                   $pid = $row["app_id"];
                 }
-                
-                echo'<tr data-tt-id="'.$row["cmp_name"].'" data-tt-parent-id="'.$row["app_id"].'">
+                if(in_array($row["cmp_id"],$appArray)){ //if the component is a child application,
+                                                        // it pulls the child components of that application
+                  echo'<tr data-tt-id="'.$row["cmp_id"].'" data-tt-parent-id="'.$row["app_id"].'">
                       <td>'.$row["cmp_name"].' '.$row["cmp_version"].'</td>
                       </tr>';
+                  $count = 0;
+                  while(array_key_exists($row["cmp_id"].$count,$nodeArray)){
+                    echo $nodeArray[$row["cmp_id"].$count];
+                    $count++;
+                  }
+                }elseif(!in_array($row["app_id"],$cmpArray)){ //if the component is not also an application and it's also not a 
+                                                              //component of a child application, it's set as a child of it's application
+                  echo'<tr data-tt-id="'.$row["cmp_id"].'" data-tt-parent-id="'.$row["app_id"].'">
+                      <td>'.$row["cmp_name"].' '.$row["cmp_version"].'</td>
+                      </tr>';
+                }
+
                   
               }//end while
           }//end if
@@ -94,11 +133,12 @@
 <script src="jquery.treetable.js"></script>
 
 <script>
-$("#sbomTable").treetable({expandable: true});
+ $("#sbomTable").treetable({expandable: true});
 
-$("#expandAll").click(function(){
+ $("#expandAll").click(function(){
   $("#sbomTable").treetable("expandAll");
 });
+
 </script>
 
 <?php include("./footer.php"); ?>
